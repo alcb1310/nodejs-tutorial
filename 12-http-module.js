08ceb46e2e54cc6eb35b69c888147d2881f4ddcb
Andes Court <andres@andrescourt.com>
2022-06-12 17:12:59 -0500

    http module basics

---
+++ 12-http-module.js
@@ -0,0 +1,34 @@
+// HTTP module
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/'){
+        res.end('Welcome to our home page')
+    } 
+    
+    if(req.url === '/about'){
+        res.end('Here is our short history')
+    } 
+    
+    res.end(`
+        <h1>Oops!</h1>
+        <p>We can't seem to find the page you are looking for</p>
+        <a href='/'>Home Page</a>
+    `)
+
+    // if(req.url === '/'){
+    //     res.write('Welcome to our home page')
+    //     res.end()
+    // } else if(req.url == '/about'){
+    //     res.write('Here is our short history')
+    //     res.end()
+    // } else {
+    //     res.end(`
+    //         <h1>Oops!</h1>
+    //         <p>We can't seem to find the page you are looking for</p>
+    //         <a href='/'>Home Page</a>
+    //     `)
+    // }
+})
+
+server.listen(5000)
