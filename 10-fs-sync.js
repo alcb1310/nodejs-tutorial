ea3109ba428ce3dbb3844a9639c2ce457318a6fc
Andes Court <andres@andrescourt.com>
2022-06-12 16:34:43 -0500

    sync file system

---
+++ 10-fs-sync.js
@@ -0,0 +1,14 @@
+const { readFileSync, writeFileSync } = require("fs")
+
+const first = readFileSync('./folder/first.txt', 'utf-8')
+const second = readFileSync('./folder/second.txt', 'utf-8')
+
+console.log(first, second)
+
+// Create and overwrite a file
+writeFileSync('./folder/result-sync.txt', `Here is the result : ${first}, ${second}`)
+
+// Create and append a file
+writeFileSync('./folder/result-sync.txt', `Here is the result : ${first}, ${second}\n`, {
+    flag: 'a'
+})
